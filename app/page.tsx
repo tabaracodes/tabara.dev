339753bb9716b3a0579b3b7aca0a67ec751accd8
Tabara Nosiba <tabara.nosiba@accenture.com>
2026-02-08 03:43:46 -0500

    Modified text color and added link to company reference

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       {/* <Navigation /> */}
       <div className='flex flex-row gap-60'>
         <h1 className='font-heading text-nowrap'>Tabara Nosiba</h1>
-        <p className='font-body'>I’m a frontend engineer focused on building thoughtful, user-centered digital experiences and translating complex product ideas into polished, scalable interfaces. I currently work at Accenture Technology Innovation, where I build interactive prototypes and user interfaces that help teams and clients quickly visualize and refine emerging product concepts.</p>
+        <p className='font-body'>I’m a frontend engineer focused on building thoughtful, user-centered digital experiences and translating complex product ideas into polished, scalable interfaces. I currently work at <a href="https://www.accenture.com/us-en/about/accenture-innovation" className="hover:underline text-[#5A3F2E]">Accenture Technology Innovation</a>, where I build interactive prototypes and user interfaces that help teams and clients quickly visualize and refine emerging product concepts.</p>
       </div>
     </div>
   );
